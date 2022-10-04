dee5c990d1ef47d81f52cf4a0583945e6e34ab1d
Willian Mano <willianmanoaraujo@gmail.com>
2022-10-04 16:22:44 -0300

    Add deploy settings

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'evokeNet', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'evokenet.github.io', // Usually your repo name.
+  trailingSlash: false,
+  deploymentBranch: 'deploy',
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
