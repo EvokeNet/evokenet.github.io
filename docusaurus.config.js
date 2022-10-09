5619194f35cdcba1e58668882079ca5452a91c2e
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-10-09 12:29:26 -0500

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Evoke project',
-  tagline: 'Evoke are cool',
+  title: 'Evoke Core',
+  tagline: 'Documentation',
   url: 'https://evokenet.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
