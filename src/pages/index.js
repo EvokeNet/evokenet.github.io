96f86248a340ca9154ad1d1af12bc7febe38b976
John Moravec <51122406+educationfutures@users.noreply.github.com>
2022-10-09 12:32:36 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,8 +17,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="https://evokenet.org">
+            Looking for an active game campaign? Click here.
           </Link>
         </div>
       </div>
